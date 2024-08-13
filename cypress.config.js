973a6cd10fc37fa77019455480a4a1caa285c6ce
Jérémy Chatard <jchatard@breek.fr>
2024-08-13 15:19:08 +0200

    Simplify params

--- cypress.config.js
+++ cypress.config.js
@@ -3,8 +3,6 @@ import { defineConfig } from "cypress";
 export default defineConfig({
   e2e: {
     baseUrl: "http://localhost:3000",
-    viewportWidth: 1280,
-    experimentalStudio: true,
     experimentalRunAllSpecs: true,
     experimentalMemoryManagement: true,
     setupNodeEvents(on, config) {
